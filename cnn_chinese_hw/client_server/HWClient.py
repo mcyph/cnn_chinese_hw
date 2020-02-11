dad9953d3c251365f336f615063cb90d8b493505
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-02-11 20:12:38 +1100

    started updating HWClient/HWServer with tflite engine

--- cnn_chinese_hw/client_server/HWClient.py
+++ cnn_chinese_hw/client_server/HWClient.py
@@ -2,7 +2,7 @@ from speedyrpc.client_server.shared_memory.SHMClient import SHMClient
 from speedyrpc.client_server.base_classes.ClientMethodsBase import ClientMethodsBase
 
 from cnn_chinese_hw.HWServer import HWServer as srv
-from input.toolkit.patterns.Singleton import Singleton
+from cnn_chinese_hw.toolkit.patterns.Singleton import Singleton
 
 
 class HWClient(ClientMethodsBase,
@@ -13,9 +13,9 @@ class HWClient(ClientMethodsBase,
             client_provider = SHMClient(srv)
         ClientMethodsBase.__init__(self, client_provider)
 
-    def get_cn_written_cand(self, LStrokes, id, DOpts):
+    def get_cn_written_cand(self, LStrokes, id):
         return self.send(srv.get_cn_written_cand, [
-            LStrokes, id, DOpts
+            LStrokes, id
         ])
 
 
@@ -23,4 +23,4 @@ if __name__ == '__main__':
     from pprint import pprint
 
     inst = HWClient()
-    pprint(inst.get_cn_written_cand([[[0, 5], [5, 10], [0, 5], [5, 10]]], 500, {}))
+    pprint(inst.get_cn_written_cand([[[0, 5], [5, 10], [0, 5], [5, 10]]], 500))
--- cnn_chinese_hw/client_server/HWServer.py
+++ cnn_chinese_hw/client_server/HWServer.py
@@ -1,9 +1,7 @@
 from json import loads, dumps
 
-from cnn_chinese_hw.parse_data import StrokeData
-from cnn_chinese_hw.recognizer import Recognizer
-
-from input.toolkit.list_operations.rem_dupes import rem_dupes
+from cnn_chinese_hw.recognizer.TFLiteRecognizer import TFLiteRecognizer
+from cnn_chinese_hw.toolkit.list_operations.rem_dupes import rem_dupes
 
 from speedyrpc.rpc_decorators import json_method
 from speedyrpc.client_server.base_classes.ServerMethodsBase import ServerMethodsBase
@@ -17,70 +15,20 @@ class HWServer(ServerMethodsBase):
     name = 'hw'
 
     def __init__(self, logger_client):
-        print('Loading stroke data...')
-        self.stroke_data = stroke_data = StrokeData()
-        print('Creating slow old recognizer instance...')
-        self.recognizer = Recognizer(stroke_data)
-        if USE_ZINNIA:
-            print('Creating zinnia recognizer instance...')
-            from cnn_chinese_hw.recognizer.ZinniaRecognizer import ZinniaRecognizer
-            self.zinnia_recognizer = ZinniaRecognizer()
-        print('OK!')
-
+        self.recognizer = TFLiteRecognizer()
         ServerMethodsBase.__init__(self, logger_client)
 
     @json_method
-    def get_cn_written_cand(self, LStrokes, id, DOpts):
-
+    def get_cn_written_cand(self, LStrokes, id):
         if not LStrokes:
-            # HACK!
-            return dumps({'LCands': LStrokes, 'id': id})
+            # TODO: Also handle single stroke candidates differently!
+            return {'LCands': [], 'id': id}
 
-        #DOpts = json_tools.loads(DOpts)
-
-        # LPoints = []
-        # for LStroke in LStrokes:
-        #    LStroke = points_to_plot([LStroke])[0]
-        #    if not LStroke: continue
-        #
-        #    Line1 = get_vertex(LStroke)
-        #    LPoints.append(Line1)
-        # LPoints = points_normalized(LPoints)
-
-        print('Recognizing:', LStrokes)
-
-        #DMap = {
-        #    'CnUnified': ['cn', 'ja'],
-        #    'CnSimp': ['cn'],
-        #    'CnTrad': ['ja'],  # FIXME!
-        #    'Japanese': ['ja']
-        #}
-
-        #t = Timer()
-        if USE_ZINNIA:
-            LRtn = [
-                chr_ for _, chr_ in
-                self.zinnia_recognizer.recognize_out_of_order(
-                    'ja', # HACK!!! ==========================================================================
-                    LStrokes
-                )
-            ]
-        else:
-            LRtn = [
-                chr(ord_) for _, ord_ in
-                self.recognizer.get_L_cands(
-                    LStrokes, DOpts
-                )[::-1]
-            ]
+        LRtn = [
+            chr(ord_) for _, ord_ in
+            self.recognizer.get_L_candidates(LStrokes)[::-1]
+        ]
 
         LRtn = rem_dupes(LRtn)
-        #print('CnWrittenCand Get Candidates:', t)
         DRtn = {'LCands': LRtn, 'id': id}
         return DRtn
-
-
-if __name__ == '__main__':
-    server = HWServer()
-    while 1:
-        from time import sleep
-        sleep(10)
