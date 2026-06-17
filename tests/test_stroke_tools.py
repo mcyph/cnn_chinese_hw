98034d4ec0381e8353872ae84dfdab0131351daf
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2026-06-18 01:29:50 +1000

    various updates+cleanups

---
+++ tests/test_stroke_tools.py
@@ -0,0 +1,30 @@
+from cnn_chinese_hw.client_server.rem_dupes import rem_dupes, fast_rem_dupes
+from cnn_chinese_hw.stroke_tools.brensenham_line import brensenham_line
+from cnn_chinese_hw.stroke_tools.get_vertex import get_vertex
+from cnn_chinese_hw.stroke_tools.points_normalized import points_normalized
+
+
+def test_rem_dupes_preserves_first_occurrence_order():
+    values = ["你", "好", "你", "學", "好", "習"]
+
+    assert rem_dupes(values) == ["你", "好", "學", "習"]
+    assert sorted(fast_rem_dupes(values)) == ["你", "好", "學", "習"]
+
+
+def test_points_normalized_scales_to_requested_bounds_and_handles_flat_axis():
+    vertical = [[(5, 2), (5, 7)]]
+    diagonal = [[(10, 10), (20, 30)]]
+
+    assert points_normalized(vertical, width=10, height=20) == [[(0, 0), (0, 20)]]
+    assert points_normalized(diagonal, width=10, height=20) == [[(0, 0), (10, 20)]]
+
+
+def test_brensenham_line_preserves_requested_direction_and_endpoints():
+    assert brensenham_line(0, 0, 3, 0) == [(0, 0), (1, 0), (2, 0), (3, 0)]
+    assert brensenham_line(3, 0, 0, 0) == [(3, 0), (2, 0), (1, 0), (0, 0)]
+    assert brensenham_line(0, 0, 0, 3) == [(0, 0), (0, 1), (0, 2), (0, 3)]
+
+
+def test_get_vertex_collapses_straight_lines_but_preserves_corners():
+    assert get_vertex([(0, 0), (250, 0), (500, 0), (1000, 0)]) == [(0, 0), (1000, 0)]
+    assert get_vertex([(0, 0), (500, 0), (500, 500)]) == [(0, 0), (500, 0), (500, 500)]
